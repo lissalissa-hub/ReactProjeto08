e9e9903350ff0206fb36b9708bd9e31c74049575
lissalissa-hub <melissadias748@gmail.com>
2024-10-20 18:42:12 -0300

    Atividade08

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Usuario from './components/Usuario'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <Usuario online ={true} />
+    <Usuario online={false}/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Usuario.jsx
@@ -0,0 +1,5 @@
+function Usuario (props){
+   return <h1>{props.online?"Usuário On-line":"Usuário Off-line"} </h1>
+}
+
+export default Usuario 
